b1551471b3c1e9e99dc64bbf6bde6eb66e579bf0
Anton Stakhouski <antonstakhouski@gmail.com>
2017-10-30 19:15:15 +0300

    large formula

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 import numpy as np
 import os
 import matplotlib.image as mpimg
-from functools import reduce
 import random
 import scipy.spatial.distance as dist
+import operator
 
 
 class ConcurrentNetwok:
     def __init__(self):
         self.side = 6
         self.n = 3
-        self.entry_layer = np.zeros(self.side ** 2)
-        self.neurons = np.zeros(self.n)
-        self.weights = np.zeros((self.side ** 2, self.n))
-        self.beta = 10
+        self.x = np.zeros(self.side ** 2)
+        self.y = np.zeros(self.n)
+        self.w = np.zeros((self.side ** 2, self.n))
+        self.b = 10
 
         self.m = 5
         self.test_images = np.zeros((self.m, self.side, self.side))
@@ -31,74 +31,74 @@ class ConcurrentNetwok:
                 self.test_images[i] = f
 
                 # normalize image vectors
-                s = np.sqrt(reduce((lambda x, y: x + y), self.test_images[i].ravel()))
+                s = np.linalg.norm(self.test_images[i].ravel())
                 for y in range(0, self.side):
                     for x in range(0, self.side):
                         self.test_images[i, y, x] = self.test_images[i, y, x] / s
                 i += 1
 
     def out_value(self, j):
-        return dist.cosine(self.entry_layer, self.weights[:, j])
+        return dist.cosine(self.x, self.w[:, j])
 
-    def init_weights(self):
-        # init weights with random values
-        for i in range(0, len(self.weights)):
-            for j in range(0, len(self.weights[0])):
-                self.weights[i, j] = random.randint(0, 10)
+    def init_w(self):
+        # init w with random values
+        for i in range(0, len(self.w)):
+            for j in range(0, len(self.w[0])):
+                self.w[i, j] = random.randint(0, 10)
 
         # normalize weight vectors
-        for i in range(0, len(self.weights)):
-            s = np.sqrt(reduce((lambda x, y: x + (y ** 2)), self.weights[i], 0))
-            self.weights[i] = list(map((lambda x: x / s), self.weights[i]))
+        for i in range(0, len(self.w)):
+            s = np.linalg.norm(self.w[i])
+            self.w[i] = list(map((lambda x: x / s), self.w[i]))
 
     def train(self):
-        self.init_weights()
+        self.init_w()
 
         for image in self.test_images:
-            self.entry_layer = image.ravel()
+            self.x = image.ravel()
 
             # find out values
-            for i in range(0, len(self.neurons)):
-                self.neurons[i] = self.out_value(i)
+            for i in range(0, len(self.y)):
+                self.y[i] = self.out_value(i)
 
             # find neuron - winner
             maximum = 0
             max_pos = 0
-            for i in range(0, len(self.neurons)):
-                if self.neurons[i] >= maximum:
-                    maximum = self.neurons[i]
+            for i in range(0, len(self.y)):
+                if self.y[i] >= maximum:
+                    maximum = self.y[i]
                     max_pos = i
 
             # powerup synaptic connections
-            for i in range(0, len(self.weights)):
-                self.weights[i, max_pos] = self.weights[i, max_pos] +\
-                                           self.beta * (self.entry_layer[i] - self.weights[i, max_pos])
+            for i in range(0, len(self.w)):
+                self.w[i, max_pos] = (self.w[i, max_pos] + self.b * (self.x[i] - self.w[i, max_pos])) /\
+                        np.linalg.norm(list(map(operator.add, self.w[:, max_pos], list(map((lambda x: self.b * x), list(map(operator.sub, self.x, self.w[:, max_pos])))))))
 
     def play(self, image):
-        neurons_t = np.array(image.ravel())
-        neurons_t1 = np.zeros(self.n)
+        y_t = np.array(image.ravel())
+        y_t1 = np.zeros(self.n)
 
         while True:
             for i in range(0, self.n):
                 value = 0
                 for j in range(0, self.n):
-                    value += self.weights[i][j] * neurons_t[j]
-                neurons_t1[i] = self.activate(value)
+                    value += self.w[i][j] * y_t[j]
+                y_t1[i] = self.activate(value)
 
             converged = True
             for i in range(0, self.n):
-                if neurons_t[i] != neurons_t1[i]:
+                if y_t[i] != y_t1[i]:
                     converged = False
                     break
             if converged:
                 break
 
-            neurons_t = neurons_t1
+            y_t = y_t1
 
         res = np.zeros((self.side, self.side, 3))
         for i in range(0, self.side):
             for j in range(0, self.side):
-                value = neurons_t1[self.side * i + j]
+                value = y_t1[self.side * i + j]
                 if value < 0:
                     value = 0
                 res[i, j] = value
